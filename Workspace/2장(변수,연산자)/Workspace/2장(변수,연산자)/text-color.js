fa773264cf7eab831ea2b979cdcbe1336a7df0e8
EMILY <92853470+SHINYEBIN@users.noreply.github.com>
2022-07-28 17:35:29 +0900

    클릭 시 글자 색깔 바뀌는 기능. 현재 시간 출력 기능

---
+++ Workspace/2장(변수,연산자)/Workspace/2장(변수,연산자)/text-color.js
@@ -0,0 +1,6 @@
+  //heading이라는 변수를 선언하고 h1태그를 저장한 것.
+  var heading=document.querySelector("#heading");
+  // h1 태그를 클릭할 경우 글자 색깔을 레드로 설정하시오.
+  heading.onclick=function(){
+      heading.style.color="red";
+  }
